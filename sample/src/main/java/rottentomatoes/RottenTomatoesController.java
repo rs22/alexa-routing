9a438c13b69e293469918dabf57d5a7c5828843e
Robert Schmid <robert.schmid@sap.com>
2016-04-06 21:48:37 -0700

    Add some functionality

--- sample/src/main/java/rottentomatoes/RottenTomatoesController.java
+++ sample/src/main/java/rottentomatoes/RottenTomatoesController.java
@@ -1,13 +1,17 @@
 package rottentomatoes;
 
+import com.google.gson.Gson;
 import com.google.inject.Inject;
 import com.google.inject.servlet.RequestScoped;
+import org.apache.http.client.fluent.Request;
 import routing.AlexaController;
 import routing.AlexaResponse;
 import routing.attributes.Slot;
 import routing.attributes.Utterances;
 import routing.providers.AlexaSessionProvider;
 import routing.providers.RequestContextProvider;
+
+import java.io.IOException;
 
 public class RottenTomatoesController extends AlexaController {
 
@@ -26,12 +30,47 @@ public class RottenTomatoesController extends AlexaController {
     @Slot({"Count"})
     public AlexaResponse topMovies(int count) {
 
-        String utterance = requestContext.getPossibleUtterance();
-        if (utterance != null && !utterance.isEmpty()) {
-            return endSessionResponse("You asked me '" + utterance + "' but I don't know the answer");
+        String moviesUri = "http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?limit=" + Integer.toString(count);
+        Movie[] movies = getMovies(moviesUri);
+
+        if (movies == null || movies.length < count) {
+            return endSessionResponse("Sorry, I don't know what the top movies are.");
         }
 
-        return endSessionResponse("I don't know what the top movies are.");
+        if (count == 1) {
+            return endSessionResponse("The top movie is " + movies[0].title);
+        }
+
+        StringBuilder response = new StringBuilder();
+        response.append("The top movies are ");
+        for (int i = 0; i < count - 1; i++) {
+            response.append(movies[i]).append(", ");
+        }
+
+        response.append("and ").append(movies[count - 1]);
+        return endSessionResponse(response.toString());
     }
 
+    private Movie[] getMovies(String url) {
+        url += url.contains("?") ? "&" : "?";
+        url += "apikey=" + "7waqfqbprs7pajbz28mqf6vz";
+
+        try {
+            String response = Request.Get(url).execute().returnContent().asString();
+            Gson gson = new Gson();
+            return gson.fromJson(response, RottenTomatoesMovies.class).movies;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private class RottenTomatoesMovies {
+        Movie[] movies;
+    }
+
+    public class Movie {
+        String id;
+        String title;
+        int year;
+    }
 }
